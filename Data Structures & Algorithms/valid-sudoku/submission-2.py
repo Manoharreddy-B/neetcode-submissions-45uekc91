9bb9fd14ac1fb9a8c2702172f78790285dda0d05
Manoharreddy-B <55451255+Manoharreddy-B@users.noreply.github.com>
2026-05-23 10:16:38 +0530

    Add: valid-sudoku - submission-2

---
+++ Data Structures & Algorithms/valid-sudoku/submission-2.py
@@ -0,0 +1,56 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        grid_dict = {}
+        for i in range(9):
+            row_set = set()
+            col_set = set()
+            for j in range(9):
+                if board[i][j] != ".":
+                    if board[i][j] not in row_set:
+                        row_set.add(board[i][j])
+                    else: 
+                        return False
+
+                if board[j][i] != ".":
+                    if board[j][i] not in col_set:
+                        col_set.add(board[j][i])
+                    else: 
+                        return False
+                
+                grid = str(i//3) +  str(j//3)
+                if board[i][j] != ".": 
+                    if grid not in grid_dict.keys():
+                        grid_dict[grid] = [board[i][j]]
+                    else:
+                        if board[i][j] not in grid_dict[grid]:
+                            grid_dict[grid].append(board[i][j])
+                        else:
+                            return False
+
+        
+        # for i in range(9):
+        #     col_set = set()
+        #     for j in range(9):
+        #         if board[j][i] not in col_set:
+        #             if board[j][i] != ".":
+        #                 col_set.add(board[j][i])
+        #             else:
+        #                 continue
+        #         else: 
+        #             return False
+        # grid_dict = {}
+        # for i in range(9):
+        #     for j in range(9):
+        #         grid = str(i//3) +  str(j//3)
+        #         if board[i][j] != ".": 
+        #             if grid not in grid_dict.keys():
+        #                 grid_dict[grid] = [board[i][j]]
+        #             else:
+        #                 if board[i][j] not in grid_dict[grid]:
+        #                     grid_dict[grid].append(board[i][j])
+        #                 else:
+        #                     return False
+        #         else:
+        #             continue
+        
+        return True
