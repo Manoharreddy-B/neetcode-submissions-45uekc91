ffd7a9082ae508ee0c12a6f3c6ae3cae394374fb
Manoharreddy-B <55451255+Manoharreddy-B@users.noreply.github.com>
2026-05-30 10:19:19 +0530

    Add: three-integer-sum - submission-2

---
+++ Data Structures & Algorithms/three-integer-sum/submission-2.py
@@ -0,0 +1,35 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        def helperTwoSum(left, right, sorted_nums, j):
+            temp_answer = []
+            while left < right:
+                target = - sorted_nums[left] - sorted_nums[right]
+                if target == sorted_nums[j-1]:
+                    temp_answer.append([sorted_nums[j-1], sorted_nums[left], sorted_nums[right]])
+                    left += 1
+                    right -= 1
+                    # continue
+                elif target > sorted_nums[j-1]:
+                    left += 1
+                else:
+                    right -= 1
+            return temp_answer
+                
+        sorted_nums = sorted(nums)
+        length = len(nums)
+        seen = set()
+        answer = []
+        # print(sorted_nums)
+        for j in range(length, 0, -1):
+            left = 0
+            right = j - 2
+            output = helperTwoSum(left, right, sorted_nums, j)
+            # print(output)
+            if output != None:
+                for lis in output:
+                    output_tuple = tuple(sorted(lis))
+                    if output_tuple not in seen:
+                        seen.add(output_tuple)
+                        answer.append(lis)
+        return answer
+        
