6b5f5ef690551fb404919fbc74c9df32b8904eaa
Manoharreddy-B <55451255+Manoharreddy-B@users.noreply.github.com>
2026-06-02 09:16:48 +0530

    Add: longest-substring-without-duplicates - submission-2

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-2.py
@@ -0,0 +1,63 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        # output = 0
+        # temp_set = set()
+        # print(len(s))
+        # for i in range(len(s)):
+        #     print(ord(s[i]))
+        #     if ord(s[i]) not in temp_set:
+        #         print(ord(s[i]))
+        #         temp_set.add(ord(s[i]))
+        #         temp_len = len(temp_set)
+        #     else:
+        #         temp_set = set()
+        #         temp_set.add(ord(s[i]))
+        #     print(temp_len)
+        #     output = max(temp_len, output)
+        # return output
+        # output = 0
+        # temp_set = set()
+        # temp_list = []
+        # i = 0
+        # while i < len(s):
+        #     if ord(s[i]) not in temp_set:
+        #         # print(ord(s[i]))
+        #         temp_list.append(ord(s[i]))
+        #         temp_set.add(ord(s[i]))
+        #         # temp_len = len(temp_list)
+        #         print(ord(s[i]), temp_list)
+        #     else:
+        #         while i < len(s) and ord(s[i]) in temp_set:
+        #             print("else", ord(s[i]), temp_list)
+        #             del temp_list[0]
+        #             temp_set = set(temp_list)
+        #             i += 1
+            
+        #     temp_len = len(temp_list)
+        #     print(temp_len)
+        #     output = max(temp_len, output)
+        #     i += 1
+        # return output
+
+        output = 0
+        temp_set = set()
+        temp_list = []
+        i = 0
+        while i < len(s):
+            if s[i] not in temp_set:
+                temp_list.append(s[i])
+                temp_set.add(s[i])
+                i += 1
+            else:
+                while i < len(s) and s[i] in temp_set:
+                    removed_char = temp_list[0]
+                    del temp_list[0]
+                    temp_set.remove(removed_char)
+
+                temp_list.append(s[i])
+                temp_set.add(s[i])
+                i += 1
+            
+            temp_len = len(temp_list)
+            output = max(temp_len, output)
+        return output
